bca43b8679409790658af376ee4a2d29d9b9ca89
kiethopt <tuankietttt17@gmail.com>
2024-12-31 20:10:54 +0700

    bổ sung page.tsx search

---
+++ frontend/src/app/(main)/search/page.tsx
@@ -0,0 +1,7 @@
+export default function SearchPage() {
+  return (
+    <div className="flex items-center justify-center h-full">
+      <h1 className="text-2xl font-bold text-white">Search Page</h1>
+    </div>
+  );
+}
