7b68a2923443f15108164809e2f397f87001914f
Duciel <146404644+DucielWork29@users.noreply.github.com>
2025-03-29 22:32:02 +0700

    Update Header.tsx

--- frontend/src/components/layout/Header/Header.tsx
+++ frontend/src/components/layout/Header/Header.tsx
@@ -369,9 +369,9 @@ export default function Header({
 
             {/* Link Sự Kiện */}
             <Link
-              href="/events"
+              href="/event/event-total"
               className={`flex items-center gap-2 text-sm font-medium px-4 py-2 rounded-md ${
-                isActive('/events')
+                isActive('/event/event-total')
                   ? theme === 'light'
                     ? 'text-gray-900 bg-gray-200'
                     : 'text-white bg-[#282828]'
